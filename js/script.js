c17408d16caeb82d1608c120fea6903d3a2845db
SmirnofOleg <super.smirnofoleg@yandex.ru>
2019-04-10 22:33:32 +0300

    решил сделать меню и контент на бутстрап и футур на флексах

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// function show() {
+// 	var menu = document.getElementById("menu");
+// 	var burger = document.getElementById("burger");
+// 	if (menu.className == "menu-main") {
+// 		// menu.style.display = "flex";
+// 		// menu.style.flexDirection = "column";
+// 		// menu.style.width = "100%";
+// 		menu.className = "menu1";
+// 		burger.className = "burger1";
+// 	} else {
+// 		// menu.style.display = "none";
+// 		// menu.style.flexDirection = "row";
+// 		menu.className = "menu2";
+// 		burger.className = "burger";
+
+// 	}
+// }
